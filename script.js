16a199384fb93c0c96a11bcacf2a381cb86705ed
salman <tangersalman14@gmail.com>
2025-09-18 17:01:07 +0100

    12 - Prototype : Décompteur de compétences Solicode

---
+++ script.js
@@ -0,0 +1,11 @@
+let cod = ["c1", "c1", "c3", "c9", "c8", "c7", "c7", "c5", "c2", "c9", "c3"]
+let computors = {};
+for(let i = 0; i<cod.length; i++) {
+    let code=cod[i]
+    if(computors[code] == undefined){
+        computors[code] = 1;
+    } else {
+        computors[code]++;
+    }
+}
+console.log(computors)
